ce6d9b298240a17ab384f9abedcda46f5626c177
guil <g.bams@student.maastrichtuniversity.nl>
2022-02-06 23:15:58 +0100

    removed the id from the componentEnum

--- src/main/java/org/group7/model/component/ComponentEnum.java
+++ src/main/java/org/group7/model/component/ComponentEnum.java
@@ -1,16 +1,14 @@
 package org.group7.model.component;
 
 public enum ComponentEnum {
-    GUARD_COMPONENT(0, ""),
-    INTRUDER_COMPONENT(1, ""),
-    WALL_COMPONENT(2, ""),
-    TELEPORTER_COMPONENT(3, ""),
-    SHADED_AREA_COMPONENT(4, "");
+    GUARD_COMPONENT(""),
+    INTRUDER_COMPONENT(""),
+    WALL_COMPONENT(""),
+    TELEPORTER_COMPONENT(""),
+    SHADED_AREA_COMPONENT("");
 
-    private final int id;
     private final String texture;
 
-    ComponentEnum(int id, String texture){ this.id = id; this.texture = texture;}
-    public int getId(){return id;}
+    ComponentEnum(String texture){this.texture = texture;}
     public String getTexture(){return texture;}
 }
