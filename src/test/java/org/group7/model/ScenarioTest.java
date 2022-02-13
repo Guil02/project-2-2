3b0c4ee7e0778797f537c5ef8bd36e30380a4756
LAPTOP-6B40E7JD\samgl <samglassman@protonmail.com>
2022-02-13 20:48:05 +0100

    fixed tests

--- src/test/java/org/group7/model/ScenarioTest.java
+++ src/test/java/org/group7/model/ScenarioTest.java
@@ -3,6 +3,8 @@ package org.group7.model;
 import org.group7.utils.Config;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,7 +15,12 @@ class ScenarioTest {
     public void testScenario(){
         String path = Config.DEFAULT_MAP_PATH;
         Scenario.setPrintInput(true);
-        Scenario scenario = new Scenario(getClass().getResource(path).getFile());
+
+        try {
+            Scenario scenario = new Scenario(new File(getClass().getResource(Config.DEFAULT_MAP_PATH).toURI()));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 
 }
