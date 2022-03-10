1c56461703bd1d5a5bce8b92bf07e65b5c5d9c43
guil <g.bams@student.maastrichtuniversity.nl>
2022-03-10 13:37:19 +0100

    improved the implementation of the graph

--- src/main/java/org/group7/model/graph/Node.java
+++ src/main/java/org/group7/model/graph/Node.java
@@ -5,6 +5,7 @@ import org.group7.model.component.staticComponents.StaticComponent;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Node {
     private final int x;
@@ -45,5 +46,26 @@ public class Node {
 
     public void addEdge(Edge edge){
         edges.add(edge);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return x == node.x && y == node.y && Objects.equals(staticComponent, node.staticComponent) && Objects.equals(playerComponent, node.playerComponent) && Objects.equals(edges, node.edges);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, staticComponent, playerComponent, edges);
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
     }
 }
