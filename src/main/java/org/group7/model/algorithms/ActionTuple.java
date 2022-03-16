3480d1e2fd597c1c5b09dfff73e9e550703e51f7
guil <g.bams@student.maastrichtuniversity.nl>
2022-03-16 16:13:30 +0100

    added ActionTuple

---
+++ src/main/java/org/group7/model/algorithms/ActionTuple.java
@@ -0,0 +1,16 @@
+package org.group7.model.algorithms;
+
+import org.group7.enums.Actions;
+
+public class ActionTuple {
+    Actions a;
+    int distance;
+    //base speed = 4
+    //ActionTuple(MOVE_FORWARD, 2)
+    //ActionTuple(TURN_DOWN, 0)
+    //ActionTuple(MOVE_FORWARD, 4)
+    //ActionTuple(MOVE_FORWARD, 1)
+    //ActionTuple(TURN_UP, 0)
+
+    // [MOVE_FORWARD, MOVE_FORWARD, TURN_down,move_forward, move_forward, move_forward, move_forward, move_forward, TURN_UP]
+}
