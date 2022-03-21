e226e7c70de94b835dba0b71e7c294ff97ebc706
LAPTOP-6B40E7JD\samgl <samglassman@protonmail.com>
2022-03-21 15:25:31 +0100

    cleanup

--- src/main/java/org/group7/model/GameRunner.java
+++ src/main/java/org/group7/model/GameRunner.java
@@ -26,7 +26,6 @@ public class GameRunner extends AnimationTimer {
     private List<State> states;
     private Scenario scenario;
     State currentState;
-    GameScreen gameScreen;
 
     SimulationScreen display;
 
@@ -100,36 +99,32 @@ public class GameRunner extends AnimationTimer {
             ActionTuple moveAction = player.calculateMove();
             //if the action is a move forward we want to take into account our base speed and move the number of base speed units
             if(moveAction.getAction() == Actions.MOVE_FORWARD){
-                for(int i = 0; i<moveAction.getDistance(); i++){
+                for(int i = 0; i < moveAction.getDistance(); i++){
                     Point targetPoint = startingPoint.clone();
-                    movePoint(player,targetPoint,i+1);
-                    if(i<player.getBaseSpeed() && !isOutOfBounds(targetPoint)){
-                        if(wallCollision(targetPoint)){
+                    movePoint(player,targetPoint,i + 1);
+
+                    if(i < player.getBaseSpeed() && !isOutOfBounds(targetPoint)){
+                        if(wallCollision(targetPoint)) {
                             break;
-                        }
-                        else if(playerCollision(player, targetPoint)){
+                        } else if(playerCollision(player, targetPoint)){
                             break;
-                        }
-                        else if(teleporterCollision(targetPoint)){
+                        } else if(teleporterCollision(targetPoint)){
                             Teleporter teleporter = (Teleporter) scenario.map[(int) targetPoint.getX()][(int) targetPoint.getY()].getStaticComponent();
                             Point teleportTarget = teleporter.getTarget();
                             player.teleport(teleportTarget);
                             player.updateVision();
                             updateGrid(startingPoint, targetPoint);
                             break;
-                        }
-                        else{
+                        } else {
                             player.moveAgentForward(moveAction.getAction());
                             player.updateVision();
                             updateGrid(startingPoint, targetPoint);
                         }
-                    }
-                    else{
+                    } else {
                         break;
                     }
                 }
-            }
-            else{
+            } else{
                 player.applyAction(moveAction.getAction());
                 player.updateVision();
             }
@@ -172,81 +167,16 @@ public class GameRunner extends AnimationTimer {
         }
     }
 
-    /**
-     * method that does the random movement for a provided player component. Is currently random can be modified to fit to the algorithms.
-     * @param p a player component you want to move
-     * @param type_movement 0= dont turn, 1 = turn right, 2= turn left
-     */
-    private void doRandomMovement(PlayerComponent p, Actions type_movement){
-
-    }
-
-    public boolean checkCollisionAgentWall(PlayerComponent player, int moveForwardDistance, int step) {
-        if (step+1 < moveForwardDistance) {
-            Point currentPosition = player.getCoordinates();
-            int x = (int)currentPosition.getX();
-            int y = (int)currentPosition.getY();
-            Orientation orientation = player.getOrientation();
-            switch (orientation) {
-                case UP -> {
-                    if (y-1 >= 0) {
-                        if (scenario.map[x][y-1].getPlayerComponent() != null || scenario.map[x][y-1].getType().isObstacle()) {
-                            return true;
-                        }
-                    }
-                    return false;
-                }
-                case DOWN -> {
-                    if (y+1 <= scenario.getHeight()) {
-                        if (scenario.map[x][y+1].getPlayerComponent() != null || scenario.map[x][y+1].getType().isObstacle()) {
-                            return true;
-                        }
-                    }
-                    return false;
-                }
-                case LEFT -> {
-                    if (x-1 >= 0) {
-                        if (scenario.map[x-1][y].getPlayerComponent() != null || scenario.map[x-1][y].getType().isObstacle()) {
-                            return true;
-                        }
-                    }
-                    return false;
-                }
-                case RIGHT -> {
-                    if (x+1 <= scenario.getWidth()) {
-                        if (scenario.map[x+1][y].getPlayerComponent() != null || scenario.map[x+1][y].getType().isObstacle()) {
-                            return true;
-                        }
-                    }
-                    return false;
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean checkCollisionTeleporter(PlayerComponent player) {
-        Point currentPosition = player.getCoordinates();
-        int x = (int)currentPosition.getX();
-        int y = (int)currentPosition.getY();
-        if (scenario.map[x][y].getStaticCompE() == TELEPORTER) {
-            return true;
-        }
-        return false;
-    }
-
-
-
     public double calculateCoverage() {
         int seenGrids = 0;
-        for (int i=0; i<scenario.getWidth();i++) {
-            for (int j=0; j<scenario.getHeight();j++) {
+        for (int i = 0; i < scenario.getWidth(); i++) {
+            for (int j=0; j < scenario.getHeight(); j++) {
                 if(scenario.map[i][j].explored) {
                     seenGrids++;
                 }
             }
         }
-        int totalSize = scenario.getWidth()*scenario.getHeight();
+        int totalSize = scenario.getWidth() * scenario.getHeight();
         double d1 = seenGrids;
         double d2 = totalSize;
 //        return (seenGrids/totalSize)*100;
