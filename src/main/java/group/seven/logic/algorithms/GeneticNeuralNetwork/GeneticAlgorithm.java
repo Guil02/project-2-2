3af69062b274a27d496147e8e48011f3cae41b64
guil <g.bams@student.maastrichtuniversity.nl>
2022-06-11 22:35:02 +0200

    implemented GA using my code from last project, we have to check if that's ok

--- src/main/java/group/seven/logic/algorithms/GeneticNeuralNetwork/GeneticAlgorithm.java
+++ src/main/java/group/seven/logic/algorithms/GeneticNeuralNetwork/GeneticAlgorithm.java
@@ -1,5 +1,28 @@
 package group.seven.logic.algorithms.GeneticNeuralNetwork;
 
 public class GeneticAlgorithm {
+    static final int populationSize = 20;
+    static final String fileName = "build/classes/java/main/model/GeneticAlgorithm/weights";
+    static final double mutationRate = 0.05;
+    static final int amountOfGenerations = 1000;
+    static final int inputSize = 20;
+    static final int hiddenSize = 5;
+    static final int outputSize = 6;
+    static final int chromosomeLength = inputSize * hiddenSize + hiddenSize + hiddenSize * outputSize + outputSize;
+    static final int maxTime = 1000;
+    private Population population;
+
+    public GeneticAlgorithm() {
+        population = new Population(populationSize, chromosomeLength, true);
+
+        train();
+    }
+
+    public void train() {
+        int index = 0;
+        while (index < amountOfGenerations) {
+            index++;
+        }
+    }
 
 }
--- src/main/java/group/seven/utils/Methods.java
+++ src/main/java/group/seven/utils/Methods.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Methods {
 
@@ -21,7 +22,8 @@ public class Methods {
     /**
      * Static import this method as a shorthand for System.out.println()
      * if debug disabled, then it won't print
-     * @param t the text, number, object or other to be printed
+     *
+     * @param t   the text, number, object or other to be printed
      * @param <T> generic parameter to enable printing of anything
      */
     public static <T> void print(T t) {
@@ -31,11 +33,37 @@ public class Methods {
     /**
      * Static import this method as a shorthand for System.out.println()
      * Set debug flag to override Config.DEBUG_MODE value
-     * @param t the object or primitive to be printed
+     *
+     * @param t     the object or primitive to be printed
      * @param debug if true then prints, else won't print.
-     * @param <T> generic parameter to enable printing of anything
+     * @param <T>   generic parameter to enable printing of anything
      */
     public static <T> void print(T t, boolean debug) {
         if (debug) System.out.println(t);
+    }
+
+
+    /**
+     * a function to generate random numbers
+     *
+     * @param min lower bound for random number
+     * @param max upper bound for random number
+     * @return the random number generated
+     */
+    public static double randomNumber(double min, double max) {
+        double interval = max - min;
+        return Math.random() * interval + min;
+    }
+    //TODO implement
+
+    /**
+     * This method will read in the stored weights of all the individuals in the genetic algorithm's population
+     * in the current generation of training.
+     *
+     * @param fileName the path to the file containing the weights of the GA
+     * @return a list containing the chromosome for each individual
+     */
+    public static ArrayList<ArrayList<Double>> readGAWeights(String fileName) {
+        throw new UnsupportedOperationException("Operation not implemented yet");
     }
 }
